6930d551b5a35751c66671cab040029b3a41d31e
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-05-12 17:28:33 -0600

    Refactor tests

--- test/Card-test.js
+++ test/Card-test.js
@@ -3,33 +3,33 @@ const expect = chai.expect;
 
 const Card = require('../src/Card');
 
-describe('Card', function() {
+describe('Card', ()=> {
 
-  let card
+    let card
 
   beforeEach(() => {
 
-  card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
 
   });
-
-  it('should be a function', function() {
+  it('should be a function', ()=> {
     expect(Card).to.be.a('function');
-  });
 
-  it('should be an instance of Card', function() {
+  });
+  it('should be an instance of Card', ()=> {
     expect(card).to.be.an.instanceof(Card);
-  });
 
-  it('should store a question', function() {
+  });
+  it('should store a question', ()=> {
     expect(card.question).to.equal('What is Robbie\'s favorite animal');
-  });
 
-  it('should store a list of possible answers', function() {
+  });
+  it('should store a list of possible answers', ()=> {
     expect(card.answers).to.deep.equal(['sea otter', 'pug', 'capybara']);
-  });
 
-  it('should store the correct answer', function() {
+  });
+  it('should store the correct answer', ()=> {
     expect(card.correctAnswer).to.equal('sea otter');
+    
   });
 });
