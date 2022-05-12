68be50f6ff261641d9f050df4cc3e2105bdbbd2d
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-05-12 17:03:29 -0600

    Add test and refactor

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -5,20 +5,23 @@ const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 
-describe("Deck", function(){
+describe("Deck", ()=> {
 
-  it("should be a function", function(){
+    beforeEach(() => {
 
-    expect(Deck).to.be.a("function");
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    deck = new Deck([card1, card2, card3]);
+});
+    it("should be a function", ()=> {
+        expect(Deck).to.be.a("function");
   });
-  it("should count cards", function(){
-
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-
-    let deck = new Deck([card1, card2, card3]);
-
-    expect(deck.countCards()).to.equal(3)
+    it("hold cards", ()=> {
+        expect(deck.cards.length).to.equal(3)
+  })
+    it("should count cards", ()=> {
+        expect(deck.countCards()).to.equal(3)
   })
 });
