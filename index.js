f3d597e6b66a001f93b591a5a69aeccf7d980bf1
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-05-12 22:26:09 -0600

    Add index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // This is where your project starts.
 
-console.log('Your project is running...'); 
+const Turn = require('./src/Turn');
+const Card = require('./src/Card');
+const Deck = require('./src/Deck');
+const Round = require('./src/Round');
+const Game = require('./src/Game');
+
+
+console.log('Your project is running...');
+
+let game = new Game(0)
+
+game.start()
